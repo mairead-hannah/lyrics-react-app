813f0f24caafe5730d2f8cdd59c21dd17ae3fd55
mairead-hannah <mairead-hannah@github.com>
2020-07-22 20:16:52 +0100

    text now displays in separate lines, however paragraphs not yet formatted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import SearchBox from './component/SearchBox'
 
+
 export default class App extends Component {
   state = {  }
   render() { 
     return ( 
       <>
       <SearchBox/>
+      
       
       </>
     );
--- src/component/SearchBox/SearchBox.jsx
+++ src/component/SearchBox/SearchBox.jsx
@@ -39,4 +39,3 @@ export default class SearchBox extends Component {
 
 
 
-
