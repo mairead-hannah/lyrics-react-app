75cdd5eff6d5515c0c89ab58ea42984d2611c87f
mairead-hannah <mairead-hannah@github.com>
2020-08-27 11:56:10 +0100

    Updates

---
+++ precache-manifest.6bd7fcf4a21381356a419899b70c00cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "899df7769029638f1ce18d20c137ad86",
+    "url": "/lyrics/index.html"
+  },
+  {
+    "revision": "3cf8363a7f0d2f5074a3",
+    "url": "/lyrics/static/css/main.723ac1e5.chunk.css"
+  },
+  {
+    "revision": "9786c0c376c87fa80bf2",
+    "url": "/lyrics/static/js/2.2a6b69a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lyrics/static/js/2.2a6b69a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cf8363a7f0d2f5074a3",
+    "url": "/lyrics/static/js/main.440234c4.chunk.js"
+  },
+  {
+    "revision": "c830f759fbe69aa6edf2",
+    "url": "/lyrics/static/js/runtime-main.c3b022fc.js"
+  },
+  {
+    "revision": "890c86cc7ca6915f8a442cedfb9e01b7",
+    "url": "/lyrics/static/media/wallpaper.890c86cc.png"
+  }
+]);
